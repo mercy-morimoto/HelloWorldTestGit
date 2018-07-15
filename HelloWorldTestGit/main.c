3e40b7dcb9518da685752d7a32b3175ad5ad388f
Masashi Morimoto <morimoto@aitech.ac.jp>
2018-07-15 14:23:17 +0900

    Initial Commit

---
+++ HelloWorldTestGit/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloWorldTestGit
+//
+//  Created by Masashi Morimoto on 2018/07/15.
+//  Copyright © 2018年 AIT. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
